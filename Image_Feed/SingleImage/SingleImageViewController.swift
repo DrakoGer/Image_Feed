b41e0263736c57df8aa5b4f62f514b14372511e2
DrakoGer <gije.ger@gmail.com>
2025-01-17 21:02:14 +0400

    Добавлен переход экрана

---
+++ Image_Feed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  Image_Feed
+//
+//  Created by Yura on 17.01.25.
+//
+
+import UIKit
+
+class SingleImageViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
