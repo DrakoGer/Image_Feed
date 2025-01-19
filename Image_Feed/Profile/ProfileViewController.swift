d1f3cfd8dc34b0b7f29084a391c4fc2c518fd5f7
DrakoGer <gije.ger@gmail.com>
2025-01-19 16:28:38 +0400

    Выполнена задача по спринту 9

--- Image_Feed/Profile/ProfileViewController.swift
+++ Image_Feed/Profile/ProfileViewController.swift
@@ -1,13 +1,66 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-    @IBOutlet private var avatarImageView: UIImageView!
-    @IBOutlet private var nameLabel: UILabel!
-    @IBOutlet private var loginNameLabel: UILabel!
-    @IBOutlet private var descriptionLabel: UILabel!
-
-    @IBOutlet private var logoutButton: UIButton!
-
-    @IBAction private func didTapLogoutButton() {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+        let avatarImage = UIImageView()
+        avatarImage.image = UIImage(named: "UserAvatar")
+        avatarImage.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(avatarImage)
+        
+        let nameLabel = UILabel()
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.textColor = UIColor(named: "YP White")
+        nameLabel.font = .boldSystemFont(ofSize: 23)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        
+        let emailLabel = UILabel()
+        emailLabel.text = "@ekaterina_nov"
+        emailLabel.textColor = UIColor(named: "YP Grey")
+        emailLabel.font = .systemFont(ofSize: 13)
+        emailLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(emailLabel)
+        
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "Hello, world!"
+        descriptionLabel.textColor = UIColor(named: "YP White")
+        descriptionLabel.font = .systemFont(ofSize: 13)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+        
+        let logoutButton = UIButton()
+        logoutButton.setImage(UIImage(named: "Logout"), for: .normal)
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        logoutButton.addTarget(self, action: #selector(logoutButtonTapped), for: .touchUpInside)
+        view.addSubview(logoutButton)
+        
+        NSLayoutConstraint.activate([
+            
+            avatarImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            avatarImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            
+            nameLabel.topAnchor.constraint(equalTo: avatarImage.bottomAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            
+            emailLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            emailLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 8),
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            
+            logoutButton.widthAnchor.constraint(equalToConstant: 24),
+            logoutButton.heightAnchor.constraint(equalToConstant: 24),
+            logoutButton.centerYAnchor.constraint(equalTo: avatarImage.centerYAnchor),
+            logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            
+        ])
+    
     }
+    
+    @objc private func logoutButtonTapped() {
+        
+        }
 }
